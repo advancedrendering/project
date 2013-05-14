cc857e50807bd68ca8d7df0a0c9d5158b24131dd
richard <richard@richard-PC>
2013-05-15 00:12:29 +0200

    - fps counter

--- ExtraBees/src/templates/MainTemplate.java
+++ ExtraBees/src/templates/MainTemplate.java
@@ -1,6 +1,5 @@
 package templates;
 
-
 /**
  * @author Andreas Elsner / Stephan Arens / Gitta Domik
  * 
@@ -9,65 +8,74 @@ package templates;
  * Research Group of Prof. Gitta Domik - Computer Graphics, Visualization and Digital Image Processing
  */
 
-
 import java.awt.event.KeyEvent;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 
+import com.sun.opengl.util.GLUT;
+
 import scenegraph.SceneRoot;
 
-public class MainTemplate extends JoglTemplate
-{
-	
+public class MainTemplate extends JoglTemplate {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	static final float[] MOVING_LIGHT_ADS = { 0.2f, 0.2f, 0.2f, 1f, 0.95f, 0.95f,
-			0.95f, 1f, 0.95f, 0.95f, 0.95f, 1f };
-	
+	static final float[] MOVING_LIGHT_ADS = { 0.2f, 0.2f, 0.2f, 1f, 0.95f,
+			0.95f, 0.95f, 1f, 0.95f, 0.95f, 0.95f, 1f };
+
 	private int frameCounter = 0;
 
-	private boolean animation = true,
-					keyPressedW = false,
-					keyPressedS = false,
-					keyPressedA = false,
-					keyPressedD = false,
-					keyPressedQ = false,
-					keyPressedE = false;
+	private boolean animation = true, keyPressedW = false, keyPressedS = false,
+			keyPressedA = false, keyPressedD = false, keyPressedQ = false,
+			keyPressedE = false;
 	private float movementSpeed = 0.5f;
 
+	private float lastTime = 0f;
+
+	private float timePerFrame = 0f;
 	
-	public static void main(String[] args)
-	{
+	private boolean showFPS = false;
+
+	public static void main(String[] args) {
 		MainTemplate assignment = new MainTemplate();
+		assignment.setSize(1280, 720);
 		assignment.setVisible(true);
 	}
 
-	public void init(GLAutoDrawable drawable)
-	{
+	public void init(GLAutoDrawable drawable) {
 		super.init(drawable);
 		GL gl = drawable.getGL();
 		// init Cg
 		// load and compile shader
-	
+
 		// bind CGParameters to vertex and fragment program
 		// z-buffer test
 		gl.glEnable(GL.GL_DEPTH_TEST);
 		// backface culling
 		gl.glEnable(GL.GL_CULL_FACE);
 		// load mesh
-	
+		lastTime = System.nanoTime();
+
 	}
 
-	public void display(GLAutoDrawable drawable)
-	{
+	public void display(GLAutoDrawable drawable) {
+
+		float currentTime = System.nanoTime();
+		if (currentTime - lastTime >= 1000000000.0f) {
+			timePerFrame = (1000.0f / ((float) getFrameCounter()));
+//			System.out.println((1000.0f / timePerFrame));
+			resetFrameCounter();
+			lastTime = System.nanoTime();
+		}
+
+		incFrameCounter();
 		updateCamCoords();
-		// if animation is on, increase frame counter
-		if (animation)
-			incFrameCounter();
+		
+		// System.out.println(getFrameCounter());
 		// get the gl object
 		GL gl = drawable.getGL();
 		// set the erasing color (black)
@@ -75,6 +83,10 @@ public class MainTemplate extends JoglTemplate
 		// clear screen with the defined erasing color and depth buffer
 		gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
 		gl.glPushMatrix();
+		if (showFPS){
+			gl.glColor3f(0, 1, 0);
+			drawFPS(drawable);
+		}
 		applyMouseTranslation(gl);
 		applyMouseRotation(gl);
 
@@ -86,7 +98,7 @@ public class MainTemplate extends JoglTemplate
 		gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, MOVING_LIGHT_ADS, 0);
 		gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, MOVING_LIGHT_ADS, 4);
 		gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, MOVING_LIGHT_ADS, 8);
-		
+
 		// set light properties
 		gl.glLightfv(GL.GL_LIGHT1, GL.GL_AMBIENT, MOVING_LIGHT_ADS, 0);
 		gl.glLightfv(GL.GL_LIGHT1, GL.GL_DIFFUSE, MOVING_LIGHT_ADS, 4);
@@ -96,10 +108,12 @@ public class MainTemplate extends JoglTemplate
 		float dLightHeight = 5.0f;
 		double dLightRadius = 5.0d;
 		float[] lightPos = new float[] {
-				(float) (dLightRadius * Math.cos(getFrameCounter() * 3.14 / 200.0)),
-				(float) (dLightRadius * Math.sin(getFrameCounter() * 3.14 / 200.0)),
-				dLightHeight, 1.0f };
-		float[] lightPos_Cam = new float[] {0f,5f,-5f,1f	};
+				(float) (dLightRadius * Math
+						.cos(System.currentTimeMillis() * 3.14 / 4000.0)),
+				(float) (dLightRadius * Math
+						.sin(System.currentTimeMillis() * 3.14 / 4000.0)), dLightHeight,
+				1.0f };
+		float[] lightPos_Cam = new float[] { 0f, 5f, -5f, 1f };
 		gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, lightPos, 0);
 		gl.glLightfv(GL.GL_LIGHT1, GL.GL_POSITION, lightPos_Cam, 0);
 
@@ -109,7 +123,7 @@ public class MainTemplate extends JoglTemplate
 		gl.glColor3f(1f, 1f, 1f);
 		getGlu().gluSphere(getGlu().gluNewQuadric(), 0.3, 10, 10);
 		gl.glPopMatrix();
-		
+
 		// draw light as sphere (without shader)
 		gl.glPushMatrix();
 		gl.glTranslatef(lightPos_Cam[0], lightPos_Cam[1], lightPos_Cam[2]);
@@ -118,24 +132,31 @@ public class MainTemplate extends JoglTemplate
 		gl.glPopMatrix();
 
 		SceneRoot.getInstance(drawable).render(drawable);
-		
+
 		gl.glPopMatrix();
 	}
 
+	private void drawFPS(GLAutoDrawable drawable) {
+		GL gl = drawable.getGL();
+		// Farbe Weiß, für die DevStrings
+		gl.glWindowPos2d(5, 5);
+		getGlut().glutBitmapString(GLUT.BITMAP_TIMES_ROMAN_24, "FPS: " + (1000.0f / timePerFrame));
+	}
+
 	private void updateCamCoords() {
-		if(keyPressedA)
-			setView_transx(getView_transx()+movementSpeed);
-		if(keyPressedD)
-			setView_transx(getView_transx()-movementSpeed);
-		if(keyPressedQ)
-			setView_transy(getView_transy()+movementSpeed);
-		if(keyPressedE)
-			setView_transy(getView_transy()-movementSpeed);
-		if(keyPressedW)
-			setView_transz(getView_transz()+movementSpeed);
-		if(keyPressedS)
-			setView_transz(getView_transz()-movementSpeed);
-		
+		if (keyPressedA)
+			setView_transx(getView_transx() + movementSpeed);
+		if (keyPressedD)
+			setView_transx(getView_transx() - movementSpeed);
+		if (keyPressedQ)
+			setView_transy(getView_transy() + movementSpeed);
+		if (keyPressedE)
+			setView_transy(getView_transy() - movementSpeed);
+		if (keyPressedW)
+			setView_transz(getView_transz() + movementSpeed);
+		if (keyPressedS)
+			setView_transz(getView_transz() - movementSpeed);
+
 	}
 
 	/**
@@ -143,82 +164,59 @@ public class MainTemplate extends JoglTemplate
 	 * 
 	 * @see getFrameCounter()
 	 */
-	private void incFrameCounter()
-	{
-		if (frameCounter < Integer.MAX_VALUE)
-		{
+	private void incFrameCounter() {
+		if (frameCounter < Integer.MAX_VALUE) {
 			frameCounter++;
-		}
-		else
+		} else
 			frameCounter = 0;
+
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e)
-	{
+	public void keyPressed(KeyEvent e) {
 		super.keyPressed(e);
-		if (e.getKeyCode() == KeyEvent.VK_A)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_F) {
+			showFPS = !showFPS;
+		} else if (e.getKeyCode() == KeyEvent.VK_A) {
 			keyPressedA = true;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_D)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_D) {
 			keyPressedD = true;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_Q)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_Q) {
 			keyPressedQ = true;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_E)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_E) {
 			keyPressedE = true;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_W)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_W) {
 			keyPressedW = true;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_S)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_S) {
 			keyPressedS = true;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			animation = !animation;
 			System.out.println("Animation: " + animation);
 		}
 	}
-	
-	public void keyReleased(KeyEvent e)
-	{
+
+	public void keyReleased(KeyEvent e) {
 		super.keyPressed(e);
-		if (e.getKeyCode() == KeyEvent.VK_A)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_A) {
 			keyPressedA = false;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_D)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_D) {
 			keyPressedD = false;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_Q)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_Q) {
 			keyPressedQ = false;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_E)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_E) {
 			keyPressedE = false;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_W)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_W) {
 			keyPressedW = false;
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_S)
-		{
+		} else if (e.getKeyCode() == KeyEvent.VK_S) {
 			keyPressedS = false;
 		}
 	}
 
-	public int getFrameCounter()
-	{
-		return frameCounter;
+	public int getFrameCounter() {
+		return this.frameCounter;
+	}
+
+	public void resetFrameCounter() {
+		this.frameCounter = 0;
 	}
 }
