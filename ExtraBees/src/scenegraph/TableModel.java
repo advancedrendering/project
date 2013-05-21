57b8abb4d7e902137df5eb9fd934cbc93ab1ae70
advancedrendering <maarten@mail.uni-paderborn.de>
2013-05-22 01:00:16 +0200

    merged

--- ExtraBees/src/scenegraph/TableModel.java
+++ ExtraBees/src/scenegraph/TableModel.java
@@ -13,7 +13,6 @@ import com.sun.opengl.util.texture.TextureIO;
 public class TableModel extends SceneGraphNode {
 	
 	private GlassModel glass = null;
-	private TempleModel temple = null;
 	private EiffelModel eiffel = null;
 	
 	private Texture tex = null;
@@ -22,16 +21,13 @@ public class TableModel extends SceneGraphNode {
 	public TableModel(GLAutoDrawable drawable, float scale) {
 		super(drawable, "models/table", scale);	
 		
-		glass = new GlassModel(drawable, scale*0.1f);
-		glass.setTranslation(0.8f, 0.832f, 0);
+		glass = new GlassModel(drawable, scale);
+//		glass.setTranslation(0.8f, 0.832f, 0);
 		this.addChild(glass);
 		
-		temple = new TempleModel(drawable, scale*0.2f);
-		temple.setTranslation(-0.8f, 0.832f, 0);
-		this.addChild(temple);
+
 		
-		eiffel = new EiffelModel(drawable, scale* 0.05f);
-		eiffel.setTranslation(1f, 0f, 2f);
+		eiffel = new EiffelModel(drawable, scale);
 		this.addChild(eiffel);
 		this.init(drawable);
 		this.setUse_vertex_shader(false);
