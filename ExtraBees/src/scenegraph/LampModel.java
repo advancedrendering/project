26d99452421371cb7367c005720ed0955983a1a7
richard <richard@richard-PC>
2013-05-21 22:42:36 +0200

    - fixed lamp model/materials (materials not final)

--- ExtraBees/src/scenegraph/LampModel.java
+++ ExtraBees/src/scenegraph/LampModel.java
@@ -2,10 +2,11 @@ package scenegraph;
 
 import javax.media.opengl.GLAutoDrawable;
 
+
 public class LampModel extends SceneGraphNode {
 	public LampModel(GLAutoDrawable drawable, float scale) {
-		super(drawable, "models/lamp", scale);
-		// TODO Auto-generated constructor stub
+		super(drawable, "models/lamp_pillar", scale);
+		this.addChild(new LampLightModel(drawable, scale));
 	}
 
 	@Override
@@ -29,5 +30,40 @@ public class LampModel extends SceneGraphNode {
 	@Override
 	public void draw(GLAutoDrawable drawable) {
 		drawable.getGL().glCallList(this.getObjectList());
+	}
+	
+	
+	class LampLightModel extends SceneGraphNode{
+
+		public LampLightModel(GLAutoDrawable drawable, float scale) {
+			super(drawable, "models/lamp_light", scale);
+			// TODO Auto-generated constructor stub
+		}
+
+		
+		@Override
+		public void init(GLAutoDrawable drawable) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void bindParameters() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void animate(GLAutoDrawable drawable) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void draw(GLAutoDrawable drawable) {
+			drawable.getGL().glCallList(this.getObjectList());
+			
+		}
+		
 	}
 }
