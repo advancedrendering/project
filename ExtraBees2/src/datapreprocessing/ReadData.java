496d5cc1503534b664add748175a82504fecd64a
lisijia0549 <lisijia@mail.uni-paderborn.de>
2013-06-13 13:44:19 +0200

    Add some stuffs...

--- ExtraBees2/src/datapreprocessing/ReadData.java
+++ ExtraBees2/src/datapreprocessing/ReadData.java
@@ -1,5 +1,27 @@
 package datapreprocessing;
 
-public class ReadData {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
+public class ReadData {
+	
+	private String filename;
+	private boolean fileExisted=false;
+	
+	
+	public ReadData(String filename)
+	{
+		this.filename=filename;
+	}
+	
+
+	public boolean fileExisted()
+	{
+		File dataFile =new File(filename);
+		fileExisted = dataFile.exists();
+		return fileExisted;
+	}
+	
+	
 }
---
+++ ExtraBees2/src/fileloc/filelocation.java
@@ -0,0 +1,16 @@
+package fileloc;
+
+/*@author Sijia
+ * This class is used for saving the locations of your data files. Because our data files will
+ * not upload to our git, so each one needs to modify it based on your own file location.
+ * 
+ * Warning: Please don't commit this file when pushing to git!!
+ */
+
+
+public class filelocation {
+	static final String bbexport = "/Users/yangmin/Downloads/bbexport-wiz2 - Copy.csv";
+	static final String nf_chuck1 = "/Users/yangmin/Downloads/nf/nf-chunk1.csv";
+	static final String nf_chuck2 = "/Users/yangmin/Downloads/nf/nf-chunk2.csv";
+	static final String nf_chuck3 = "/Users/yangmin/Downloads/nf/nf-chunk3.csv";
+}
